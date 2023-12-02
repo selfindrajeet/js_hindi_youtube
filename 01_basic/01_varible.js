c2a65a8e8dff26a018812db411e637478a31e220
selfindrajeet <108279727+selfindrajeet@users.noreply.github.com>
2023-12-02 17:21:54 +0000

    datatype

---
+++ 01_basic/01_varible.js
@@ -0,0 +1,15 @@
+const accountId = 12458
+let accountEmail = "bittukumar1022005@yahoo.com"
+var accountPassword = "123456"
+//accountCity - "himachal Pradesh"
+
+//accountId = 24
+
+accountEmail = "abc@gmail.com"
+accountPassword = "121212"
+//accountCity = "Bengaluru"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, ]);
+
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3  + 3) // we are using nodejs , not browser
+
+console.log(3 + 3); 
+
+console.log("Bittu")
+
+let name = "Bittu"
+let age = 21
+let isLoggedIn = false
+let stste;
+
+// number => 2 to powe 53
+// bigint
+// string => ""
+//boolean => true/false
+//null = standalone value
+//undefined => 
+//symbol => unique
+
+
+//object
+console.log(typeof undefined); //undefined 
+
+console.log(typeof null); // object
