1ae528876cf841135577c29b4db7f680eec3635e
selfindrajeet <108279727+selfindrajeet@users.noreply.github.com>
2023-12-12 07:46:16 +0000

    Conversion of data type

---
+++ 01_basic/03_conversionOpration.js
@@ -0,0 +1,27 @@
+let score = "36abc"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33"  => 33
+// "33abc"  => NaN
+// true  =>  1;  false => 0
+
+let isLoggedIn = "Bittu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+// 1m=> true; 0 => false 
+//"Bittu" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
